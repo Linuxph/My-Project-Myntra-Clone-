857975a783ad5d05bfd278c69848eccc0bbb5dde
Linuxph <87934843+Linuxph@users.noreply.github.com>
2024-01-27 19:45:54 +0530

    Update index.js

--- index.js
+++ index.js
@@ -181,12 +181,12 @@ function bagitem() {
 
 function callslider() {
     document.querySelector('.slider').style.left = '0%'
-    document.querySelector('.shadow').style.left = '51%'
+    document.querySelector('.shadow').style.left = '0%'
     document.body.style.overflow = 'hidden';
 }
 
 function back() {
-    document.querySelector('.slider').style.left = '-50%';
-    document.querySelector('.shadow').style.left = '-50%';
+    document.querySelector('.slider').style.left = '-80%';
+    document.querySelector('.shadow').style.left = '-100%';
     document.body.style.overflow = 'auto';
 }
